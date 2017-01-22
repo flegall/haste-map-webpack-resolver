3f45b405e7c5b548beccbea15d95aa6767d7cf02
Florent Le Gall <florent.legall@gmail.com>
2017-01-23 00:36:22 +0100

    Remove unnecessary console.log in tests

--- packages/haste-map-webpack-resolver-demo-webpack2/__tests__/demo.test.js
+++ packages/haste-map-webpack-resolver-demo-webpack2/__tests__/demo.test.js
@@ -1,5 +1,4 @@
 test('Package dependency is succesfully imported', () => {
     const {entryPoint} = require('../dist/entry-point.bundle');
-    console.log(entryPoint());
     expect(entryPoint()).toBe('Dependency resolved');
 });
